2e975d85a695496ff56bb98f5e5e0bef26a33033
Reidar <reidar.husmo@gmail.com>
2016-02-21 20:18:56 +0100

    Jibber jabber

--- PT.PointOne/PT.PointOne.PowerShell.Lightning/LibraryLimit.ps1
+++ PT.PointOne/PT.PointOne.PowerShell.Lightning/LibraryLimit.ps1
@@ -11,21 +11,21 @@ $originalFilePath = "C:\Temp\Document.docx"
 $folderNames = ("HR","Sales","Marketing","Dev","Research","Social","Test")
 $folderCount = $folderNames.Length
 $lowerLimit = 0
-$upperLimit = 699
+$upperLimit = 722
 $items = $upperLimit - $lowerLimit
 $counter = 0
 ($lowerLimit..$upperLimit) | % {
     if ($_ % 50 -eq 0) { Connect-SPOnline $url -Credentials "Aspc1606" }
     $leaf = $_ 
-    Write-Progress -ID 1 -Activity "Uploading files $_" -PercentComplete (100*$counter / $items)
-    $counter = $counter + 1
+    Write-Progress -ID 1 -Activity "Uploading files" -Status $_ -PercentComplete (100*$counter / $items)
+    $counter++
     $innerCounter = 0
     $folderNames | % { 
         $folder = "$libraryName/$_"
         $fileName = "TestDocument$_$leaf.docx"
         $file = "C:\temp\$fileName"
-        Write-Progress -ID 2 -Activity "Uploading files" -Status "Please wait, this shouldn't take long" -PercentComplete (100*$innerCounter / $folderCount)
-        $innerCounter = $innerCounter + 1
+        Write-Progress -ID 2 -Activity "Uploading $folderCount files" -Status "Please wait, this shouldn't take long" -PercentComplete (100*$innerCounter / $folderCount)
+        $innerCounter++
         Copy-Item -LiteralPath $originalFilePath -Destination $file
         Add-SPOFile -Path $file -Folder $folder
         Remove-Item -Path $file
