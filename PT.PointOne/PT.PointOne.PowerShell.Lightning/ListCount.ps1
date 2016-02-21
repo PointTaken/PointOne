f7ed18f0aa9666b9a1eee0f851bc0d06078417cb
Reidar <reidar.husmo@gmail.com>
2016-02-21 20:24:02 +0100

    Shorter version. Because we are lazy programmers

--- PT.PointOne/PT.PointOne.PowerShell.Lightning/ListCount.ps1
+++ PT.PointOne/PT.PointOne.PowerShell.Lightning/ListCount.ps1
@@ -1,8 +1,5 @@
 #
 # ListCount.ps1
 #
-$url = "https://aspc1606.sharepoint.com/sites/dev"
-Connect-SPOnline $url -Credentials "Aspc1606"
-$libraryName = "LargeLibrary"
-$list = get-spolist $libraryName
-$list.ItemCount
+Connect-SPOnline https://aspc1606.sharepoint.com/sites/dev -Credentials Aspc1606
+Get-SPOList LargeLibrary | % { $_.ItemCount }
