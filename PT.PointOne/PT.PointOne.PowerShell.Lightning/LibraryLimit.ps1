c9403fe3594a3e349086344aef76e05aefbe1d07
Reidar Husmo <reidar.husmo@gmail.com>
2016-02-21 11:54:56 +0100

    Foldernames..

--- PT.PointOne/PT.PointOne.PowerShell.Lightning/LibraryLimit.ps1
+++ PT.PointOne/PT.PointOne.PowerShell.Lightning/LibraryLimit.ps1
@@ -3,5 +3,9 @@
 #
 Connect-SPOnline https://aspc1606.sharepoint.com/sites/large -Credentials "Aspc1606"
 
-# do we have a library?
-Get-SPOList "Documents"
+$libraryName = "Documents"
+
+$folderNames = ("HR","Sales","Marketing","Dev","Research","Social","Tant","Fjas")
+$folderNames | Foreach { 
+    $folderName = $_ 
+}
