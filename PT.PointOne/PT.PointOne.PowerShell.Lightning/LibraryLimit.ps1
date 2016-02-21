35d0cdf9f2afa52b8dc64234c70acdc966adf117
Reidar <reidar.husmo@gmail.com>
2016-02-21 17:56:14 +0100

    Final version (?)

--- PT.PointOne/PT.PointOne.PowerShell.Lightning/LibraryLimit.ps1
+++ PT.PointOne/PT.PointOne.PowerShell.Lightning/LibraryLimit.ps1
@@ -9,23 +9,23 @@ $originalFilePath = "C:\Temp\Document.docx"
 
 $folderNames = ("HR","Sales","Marketing","Dev","Research","Social","Test")
 $folderCount = $folderNames.Length
-$lowerLimit = 2
-$upperLimit = 3
+$lowerLimit = 0
+$upperLimit = 700
 $items = $upperLimit - $lowerLimit
 $counter = 0
 ($lowerLimit..$upperLimit) | % {
     $leaf = $_ 
-    $counter = $counter + 1
     Write-Progress -ID 1 -Activity "Uploading files" -PercentComplete (100*$counter / $items)
+    $counter = $counter + 1
     $innerCounter = 0
     $folderNames | % { 
         $folder = "$libraryName/$_"
         $fileName = "TestDocument$_$leaf.docx"
         $file = "C:\temp\$fileName"
-        $innerCounter = $innerCounter + 1
         Write-Progress -ID 2 -Activity "Uploading files" -Status "Uploading file" -PercentComplete (100*$innerCounter / $folderCount)
+        $innerCounter = $innerCounter + 1
         Copy-Item -LiteralPath $originalFilePath -Destination $file
-#        Add-SPOFile -Path $file -Folder "$folder"
+        Add-SPOFile -Path $file -Folder "$folder"
         Remove-Item -Path $file
     }
 }
