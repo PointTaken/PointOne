582e864e50d2c265908ec8461ba148b22757caec
Reidar <reidar.husmo@gmail.com>
2016-02-21 19:17:57 +0100

    Because the voices told me to.

--- PT.PointOne/PT.PointOne.PowerShell.Lightning/LibraryLimit.ps1
+++ PT.PointOne/PT.PointOne.PowerShell.Lightning/LibraryLimit.ps1
@@ -1,7 +1,8 @@
 ﻿#
 # LibraryLimit.ps1
 #
-Connect-SPOnline https://aspc1606.sharepoint.com/sites/dev -Credentials "Aspc1606"
+$url = "https://aspc1606.sharepoint.com/sites/dev"
+Connect-SPOnline $url -Credentials "Aspc1606"
 
 $libraryName = "LargeLibrary"
 
@@ -9,24 +10,24 @@ $originalFilePath = "C:\Temp\Document.docx"
 
 $folderNames = ("HR","Sales","Marketing","Dev","Research","Social","Test")
 $folderCount = $folderNames.Length
-$lowerLimit = 164
+$lowerLimit = 0
 $upperLimit = 699
 $items = $upperLimit - $lowerLimit
 $counter = 0
 ($lowerLimit..$upperLimit) | % {
-    if ($_ % 50 -eq 0) { Connect-SPOnline https://aspc1606.sharepoint.com/sites/dev -Credentials "Aspc1606" }
+    if ($_ % 50 -eq 0) { Connect-SPOnline $url -Credentials "Aspc1606" }
     $leaf = $_ 
-    Write-Progress -ID 1 -Activity "Uploading files" -PercentComplete (100*$counter / $items)
+    Write-Progress -ID 1 -Activity "Uploading files $_" -PercentComplete (100*$counter / $items)
     $counter = $counter + 1
     $innerCounter = 0
     $folderNames | % { 
         $folder = "$libraryName/$_"
         $fileName = "TestDocument$_$leaf.docx"
         $file = "C:\temp\$fileName"
-        Write-Progress -ID 2 -Activity "Uploading files" -Status "Uploading file" -PercentComplete (100*$innerCounter / $folderCount)
+        Write-Progress -ID 2 -Activity "Uploading files" -Status "Please wait, this shouldn't take long" -PercentComplete (100*$innerCounter / $folderCount)
         $innerCounter = $innerCounter + 1
         Copy-Item -LiteralPath $originalFilePath -Destination $file
-        Add-SPOFile -Path $file -Folder "$folder"
+        Add-SPOFile -Path $file -Folder $folder
         Remove-Item -Path $file
     }
 }
