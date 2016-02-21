3800b1dcf6bce2dc1c64138f6a749286014415c0
Reidar <reidar.husmo@gmail.com>
2016-02-21 17:29:54 +0100

    Rearrange the loops (for cryptic reasons)

--- PT.PointOne/PT.PointOne.PowerShell.Lightning/LibraryLimit.ps1
+++ PT.PointOne/PT.PointOne.PowerShell.Lightning/LibraryLimit.ps1
@@ -8,11 +8,11 @@ $libraryName = "LargeLibrary"
 $originalFilePath = "C:\Temp\Document.docx"
 
 $folderNames = ("HR","Sales","Marketing","Dev","Research","Social","Test")
-$folderNames | Foreach { 
-    $folderName = $_ 
-    (0..2) | % {
-        $folder = "$libraryName/$folderName"
-        $fileName = "TestDocument$folderName$_.docx"
+(0..2) | % {
+    $leaf = $_ 
+    $folderNames | % { 
+        $folder = "$libraryName/$_"
+        $fileName = "TestDocument$_$leaf.docx"
         $file = "C:\temp\$fileName"
         Copy-Item -LiteralPath $originalFilePath -Destination $file
 #        Add-SPOFile -Path $file -Folder "$folder/$fileName"
