c4b9fb3b787c6531ad6eb84124ce4e6a809e1f28
Reidar <reidar.husmo@gmail.com>
2016-02-21 17:54:03 +0100

    More progress. And fixed a bad.

--- PT.PointOne/PT.PointOne.PowerShell.Lightning/LibraryLimit.ps1
+++ PT.PointOne/PT.PointOne.PowerShell.Lightning/LibraryLimit.ps1
@@ -8,20 +8,24 @@ $libraryName = "LargeLibrary"
 $originalFilePath = "C:\Temp\Document.docx"
 
 $folderNames = ("HR","Sales","Marketing","Dev","Research","Social","Test")
-$lowerLimit = 0
-$upperLimit = 1000
+$folderCount = $folderNames.Length
+$lowerLimit = 2
+$upperLimit = 3
 $items = $upperLimit - $lowerLimit
 $counter = 0
 ($lowerLimit..$upperLimit) | % {
     $leaf = $_ 
-    Write-Progress -Activity "Uploading files" -PercentComplete (100*$counter / $items)
     $counter = $counter + 1
+    Write-Progress -ID 1 -Activity "Uploading files" -PercentComplete (100*$counter / $items)
+    $innerCounter = 0
     $folderNames | % { 
         $folder = "$libraryName/$_"
         $fileName = "TestDocument$_$leaf.docx"
         $file = "C:\temp\$fileName"
+        $innerCounter = $innerCounter + 1
+        Write-Progress -ID 2 -Activity "Uploading files" -Status "Uploading file" -PercentComplete (100*$innerCounter / $folderCount)
         Copy-Item -LiteralPath $originalFilePath -Destination $file
-#        Add-SPOFile -Path $file -Folder "$folder/$fileName"
+#        Add-SPOFile -Path $file -Folder "$folder"
         Remove-Item -Path $file
     }
 }
