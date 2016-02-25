b920275488dd472b13567bfa9d24c1f1f5cb087a
Kristian Oftedal <kristian.oftedal@pointtaken.no>
2016-02-25 21:58:08 +0100

    gulp

---
+++ PT.PointOne/PT.PointOne.MobileApp/gulpfile.js
@@ -0,0 +1,21 @@
+﻿var gulp = require("gulp"),
+    cordova = require("cordova-lib").cordova;       // Load gulp
+var uglify = require("gulp-uglify"); // Load gulp-uglify
+var concat = require("gulp-concat"); // Load gulp-concat
+
+gulp.task("combine-and-uglify", function () {
+    return gulp.src('www/scripts/*.js')
+        .pipe(concat('combined.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('min/scripts'));
+});
+
+
+gulp.task("default", function (callback) {
+    cordova.build({
+        "platforms": ["android"],
+        "options": {
+            argv: ["--release", "--gradleArg=--no-daemon"]
+        }
+    }, callback);
+});
