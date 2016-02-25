2ab08fe94cb7648b698a09c9f6aaf85aaff5dafd
Reidar Husmo <reidar.husmo@gmail.com>
2016-02-25 12:38:43 +0100

    Patrons created

--- PT.PointOne/PT.PointOne.PowerShell/SuperHeroes.ps1
+++ PT.PointOne/PT.PointOne.PowerShell/SuperHeroes.ps1
@@ -1,13 +1,14 @@
 #
 # SuperHeroes.ps1
 #
-$url = http://aspc1606.sharepoint.com/sites/PointOneArms
+$url = https://aspc1606.sharepoint.com/sites/PointOneArms
 
-#Connect-SPOnline -Url $url -Credentials Aspc1606
+Connect-SPOnline -Url $url -Credentials Aspc1606
 
-#$web = Get-SPOWeb
+$web = Get-SPOWeb
 
 $heroes = Get-Content "C:\Projects\PointOne\PT.PointOne\Resources\Superheroes.txt"
 $heroes | % {
-	$_
+    $units = 4 + (Get-Random 21)
+	Add-SPOListItem -List "Patrons" -Values @{ "Title" = $_; "WeeklyUnits" = $units }
 }
