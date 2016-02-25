27e88d1e61c9115bfbc888618c93dc3294be4034
Helge Smeby <helge.smeby@inmeta.com>
2016-02-25 10:54:47 +0100

    iot commit

---
+++ PT.PointOne/IOTHubInterface/Models/OrderStatusRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTHubInterface.Models
+{
+    public class OrderStatusRequest
+    {
+        public string RequestID { get; set; } 
+    }
+}
